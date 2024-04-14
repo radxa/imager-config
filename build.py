e94bf5512f2c177f27d6663ece1d26de7fcf5b41
shawnchainw <chenxuewu@radxa.com>
2024-04-14 21:29:10 +0800

    add images

---
+++ build.py
@@ -0,0 +1,70 @@
+import json
+import os
+
+
+def convert_bytes(bytes, decimals=2):
+    units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+    if bytes == 0:
+        return '0 B'
+    i = 0
+    while bytes >= 1024 and i < len(units) - 1:
+        bytes /= 1024
+        i += 1
+    return f'{bytes:.{decimals}f} {units[i]}'
+
+
+root = 'products'
+
+with open(os.path.join(root, 'config.json')) as f:
+    imager_config = json.load(f)
+
+result = {
+    "devices": [],
+    "device_images": {},
+    "images": {},
+    **imager_config
+}
+
+for serial in os.listdir(root):
+    _path = os.path.join(root, serial)
+    _config = os.path.join(_path, 'config.json')
+    if not os.path.exists(_config):
+        continue
+    with open(_config, 'r') as f:
+        serial_data = json.load(f)
+    serial_data['data'] = []
+    result['devices'].append(serial_data)
+    for device in os.listdir(_path):
+        if device != 'config.json':
+            with open(os.path.join(_path, device, 'config.json')) as f:
+                device_data = json.load(f)
+            serial_data['data'].append(device_data)
+            image_path = os.path.join(_path, device)
+            _image_result = []
+            for image in os.listdir(image_path):
+                if image != 'config.json':
+                    with open(os.path.join(image_path, image)) as f:
+                        image_data = json.load(f)
+                        _image_result.append(
+                            {
+                                'id': image_data['id'],
+                                'pic_url': image_data.get('pic_url', ''),
+                                'name': image_data['name'],
+                                'desc': image_data.get('desc', ''),
+                                'time': image_data.get('time', ''),
+                                'size': convert_bytes(image_data['size']),
+                                'edit': image_data['edit'],
+                                'edit_version': image_data.get('edit_version', 0),
+                                'hide': image_data['hide'],
+                            }
+                        )
+                        result['images'][image_data['id']] = {
+                            "checksum_method": image_data['checksum_method'],
+                            "checksum_file": image_data['checksum_file'],
+                            "checksum_url": image_data['checksum_url'],
+                            "extension": image_data['extension'],
+                            "download_url": image_data['download_url'],
+                            "size": image_data['size'],
+                        }
+            result['device_images'][device_data['id']] = [_image_result]
+print(json.dumps(result, indent=4))
--- get_file_size.py
+++ get_file_size.py
@@ -1,14 +1,30 @@
+import json
+
 import requests
 
-url = 'https://gate.radxa.com/radxa-build/radxa-zero3/releases/download/b6/radxa-zero3_debian_bullseye_xfce_b6.img.xz'
+url = 'https://github.com/radxa-build/radxa-zero3/releases/download/b6/radxa-zero3_debian_bullseye_xfce_b6.img.xz'
+date = '2024-01-10'
 
+url = url.replace('github.com', 'gate.radxa.com')
+ver = url[:-2] + 'sha512'
 res = requests.head(url, allow_redirects=True)
 length = res.headers.get('Content-Length')
 
 name = url.split('/')[-1].split('.')[0]
 _json = {
     'name': name,
-    'size': length
+    'size': int(length),
+    'pic_url': "https://imager.radxa.com/image/debian.svg",
+    "desc": "This flavor is officially supported, tested, and recommended for all users.",
+    'time': date,
+    'edit': True,
+    'edit_version': 1,
+    "hide": False,
+    "checksum_method": "sha512",
+    "checksum_file": "url",
+    "checksum_url": ver,
+    "extension": "xz",
+    "download_url": url
 }
 
-print(_json)
+print(json.dumps(_json, indent=4))
